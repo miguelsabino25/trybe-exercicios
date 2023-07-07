fdc74670ba209b9a298d855a0f42c1fce1e6feea
miguelsabino15 <miguel2012palhada@gmail.com>
2023-07-07 14:06:27 -0300

    Resolve exercício quadrado e triangulo asteristico

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/desafioAsteristico.js
@@ -0,0 +1,12 @@
+let n = 5;
+let result = "";
+
+if (n > 1) {
+  for (let index = 0; index < n; index += 1) {
+    result += "*";
+  }
+}
+
+for (let index = 0; index < n; index += 1) {
+  console.log(result);
+}
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/desafioPiramideAsteristico.js
@@ -0,0 +1,8 @@
+let n = 5;
+let result = "";
+
+if (n > 1) {
+  for (let index = 0; index < n; index += 1) {
+    console.log((result += "*"));
+  }
+}
